382d00a8ad5019c93e6cb26b4e87bf0a5c4dbfab
davton <dtonderski@gmail.com>
2022-10-31 07:12:06 -0400

    Finished working on AlexNet model

--- deep_learning_playground/papers/alexnet/model.py
+++ deep_learning_playground/papers/alexnet/model.py
@@ -66,18 +66,3 @@ class AlexNet(nn.Module):
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
-
-
-if __name__ == '__main__':
-    # test = nn.Sequential(nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
-    #                      nn.MaxPool2d(kernel_size=3, stride=2),
-    #                      nn.Conv2d(96, 256, kernel_size=5, stride=1,
-    #                                padding=2),
-    #                      nn.MaxPool2d(kernel_size=3, stride=2),
-    #                      nn.Conv2d(256, 384, kernel_size=3, stride=1,
-    #                                padding=1),
-    #                      nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
-    #                      nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1)
-    #                      )
-    test = AlexNet()
-    tensor = torch.rand((1, 3, 224, 224))
